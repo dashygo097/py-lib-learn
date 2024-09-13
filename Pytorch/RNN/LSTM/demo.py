e0d076ccb0ccb16a91805be81db4da06aa53bf66
dashygo097 <u202311806@hust.edu.cn>
2024-09-13 13:39:44 +0800

    Update demo.py

--- Pytorch/RNN/LSTM/demo.py
+++ Pytorch/RNN/LSTM/demo.py
@@ -5,43 +5,50 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 from model import Basic097, encoder
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader,TensorDataset
 
 dataset = load_dataset("./stanford-imdb/plain_text")
 tokenizer = AutoTokenizer.from_pretrained("./bert-tokenizer")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 batch_size = 4
 num_hiddens = max_length = 512
 
-model = Basic097(tokenizer.vocab_size ,num_hiddens, device , max_length = max_length)
+model = Basic097(tokenizer.vocab_size ,num_hiddens, device , max_length = max_length).to(device)
 
 loss = nn.CrossEntropyLoss()
 optimizer = torch.optim.AdamW(model.parameters() , lr = 1e-3)
 
 loss_epoch = []
 
-data_loader = DataLoader(dataset["train"] ,batch_size = batch_size , shuffle=False)
+features = encoder(tokenizer,dataset["train"]["text"],max_length=max_length)
+labels = torch.tensor(dataset["train"]["label"])
+train_data = TensorDataset(features, labels)
+data_loader = DataLoader(train_data , batch_size=batch_size  ,shuffle=False)
 
-
-def trainer(model , n_epochs):
+def trainer(model, n_epochs):
+    model.train()
     for epoch in range(n_epochs):
         loss_reg = 0
-        state = model.begin_state(batch_size , device)
-        for X,label in data_loader:
+        for X_batch, y_batch in data_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+
             optimizer.zero_grad()
-            X = encoder(tokenizer,X,max_length=max_length)
-            Y,state = model(X,state).to(device)
-            l = loss(model.predict(Y.to(device)) , state.to(device))
-            loss_reg += l
+
+            state = model.begin_state(batch_size, device=device)
+            output, state = model(X_batch.detach(), state)
+            pred = model.predict(output, device)
+
+            l = loss(pred, y_batch)
+            loss_reg += l.item()
+
             l.backward()
             optimizer.step()
 
-        loss_epoch.append(loss_reg.detech())
-        print(f"epoch: {epoch} , loss: {loss_reg}")
+        loss_reg /= len(data_loader)
+        print(f"epoch: {epoch}, loss: {loss_reg:.4f}")
 
-trainer(model,2)
+trainer(model,5)
 
 
 
